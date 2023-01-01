99e2647ebbc6780fc4151730a245cf1daae50141
Andrey <and-lukyane@yandex.ru>
2023-01-01 18:39:55 +0400

    App redesign.

--- main.py
+++ main.py
@@ -1,48 +1,189 @@
+import pandas as pd
 import streamlit as st
 from deep_translator import GoogleTranslator
 
-from src.utils import get_data_from_vocab, make_more_columns
+from src.utils import get_data_from_vocab, make_more_columns, show_vocabulary_stats
 
-st.set_page_config(layout='wide')
+st.set_page_config(page_title='Kindle Vocabulary to Anki converter')
 
 st.title('Kindle Vocabulary to Anki converter')
+if 'load_state' not in st.session_state:
+    st.session_state.load_state = False
+if 'translated_df' not in st.session_state:
+    st.session_state.translated_df = pd.DataFrame()
+text = """
+    This is an app for converting kindle vocab file into a spreadsheet that can be imported into anki.\n
+    You can change the app width or turn on the dark mode in the Settings using the button in the top right corner.\n
+    To get started, you need to have vocab.db from your Kindle device at hand. If you don't know how to get it,
+    here are the steps to do it:
+    * connect your Kindle device to your PC/laptop by cable;
+    * copy vocabulary file `Kindle/system/vocabulary/vocab.db` to your PC/laptop;
+    \n
+    If you want to read more information, open the section below.
+"""
+st.markdown(text, unsafe_allow_html=True)
 
-db = st.file_uploader('vocab.db', type='db')
-if db:
-    # get data
-    data = get_data_from_vocab(db)
+my_expander = st.expander(label='More info')
+with my_expander:
+    """
+    The idea of this app appeared when I tried to export Kindle vocabulary to Anki using my Macbook.
+    I found out that there are the following options:
+    * Kindle Mate - an awesome app, but it works for Windows only;
+    * Several repositories on GitHub, requiring installing them;
+    * Anki addon - great
+    * flashcards
 
+    As a result, I think I can make my own solution which is more customizable.
+
+    This app doesn't keep your data - it is saved only in cache and is cleared after the end of your session.
+
+    If you have any feedback, issues or ideas of improvement, you can create an issue on GitHub or sent me an e-mail.
+    """
+
+st.subheader('Upload your kindle vocabulary file here')
+
+db = st.file_uploader('vocab.db', type='db', help='Upload the vocabulary file here')
+
+use_sample = st.button('Press the button to use a sample data')
+
+data = pd.DataFrame
+
+if use_sample:
+    data = pd.read_csv('data_example/example_data.csv')
+
+if db or use_sample:
+    # use sample data or the uploaded data
+    if not use_sample:
+        data = get_data_from_vocab(db)
+
+    st.subheader('Extracted data')
+    text3 = """
+        This is the data extracted from the Kindle vocabulary file. You can sort it by clicking on any column name.
+    """
+    st.markdown(text3, unsafe_allow_html=True)
     st.dataframe(data)
-    st.subheader('Processed data')
 
-    # limit the number of rows
-    top_n: int = int(st.number_input('Take top N rows', min_value=1, max_value=data.shape[0], value=10))
-    data = data[:top_n]
+    my_expander1 = st.expander(label='Show vocabulary statistics')
+    with my_expander1:
+        show_vocabulary_stats(data)
+    st.subheader('Define translation parameters')
+    my_expander2 = st.expander(label='Translation parameters', expanded=True)
+    with my_expander2:
+        # limit the number of rows
+        col1_, col2_ = st.columns(2)
+        with col1_:
+            top_n: int = int(
+                st.number_input('Take last N rows', min_value=1, max_value=data.shape[0], value=data.shape[0])
+            )
+        with col2_:
+            col_by = st.selectbox(
+                'Sort data by', options=['Timestamp', 'Word'], help='Select the column to sort the data by'
+            )
 
-    # select the target language
-    langs_list = list(GoogleTranslator().get_supported_languages(as_dict=True).values())
-    lang = st.selectbox('Lang', options=langs_list, index=langs_list.index('en'))
+        data = data.sort_values(col_by)[-top_n:]
+        d = st.date_input(
+            label='Starting date',
+            value=pd.to_datetime(data['Timestamp']).dt.date.min(),
+            min_value=pd.to_datetime(data['Timestamp']).dt.date.min(),
+            max_value=pd.to_datetime(data['Timestamp']).dt.date.max(),
+            help='Change this value if you want to limit the data by the start date',
+        )
+        data = data.loc[pd.to_datetime(data['Timestamp']).dt.date >= d]
+        # select the target language
+        langs_list = GoogleTranslator().get_supported_languages()
+        lang = st.selectbox('Lang to translate into', options=langs_list, index=langs_list.index('english'))
+        lang = GoogleTranslator().get_supported_languages(as_dict=True)[lang]
 
-    to_translate = st.multiselect('What to translate (select one or multiple)', ['word', 'stem', 'example'], ['word'])
-    # translate and create sentences
-    data = make_more_columns(data, lang, to_translate)
+        to_translate = st.multiselect(
+            label='What to translate (select one or multiple)',
+            options=['Word', 'Stem', 'Sentence'],
+            default=['Word'],
+            help='Select the columns that will be translated',
+        )
 
-    options = st.multiselect('Columns to use', list(data.columns), list(data.columns))
+        books = st.multiselect(
+            label='books to translate',
+            options=data['Book title'].unique(),
+            default=data['Book title'].unique(),
+            help='Select the books that will be translated',
+        )
+        data = data.loc[data['Book title'].isin(books)]
+        authors = st.multiselect(
+            label='books to translate',
+            options=data['Authors'].unique(),
+            default=data['Authors'].unique(),
+            help='Select the Authors that will be translated',
+        )
+        data = data.loc[data['Authors'].isin(authors)]
 
-    # possibility to rename the columns
-    new_col_names = {}
-    for col in options:
-        new_name = st.text_input(f'{col} name', f'{col}')
-        new_col_names[col] = new_name
+        langs_from = st.multiselect(
+            label='languages to translate',
+            options=data['Word language'].unique(),
+            default=data['Word language'].unique(),
+            help='Select the languages that will be translated',
+        )
+        data = data.loc[data['Word language'].isin(langs_from)]
 
-    # downloading
-    new_data = data[options].rename(columns=new_col_names)
-    st.dataframe(new_data)
-    st.subheader('Download options')
-    sep = st.selectbox('Select separator', (';', 'Tab'))
-    sep = sep if sep == ';' else '\t'
-
-    file_name = st.text_input('File name', 'anki_table')
-    st.download_button(
-        'Press to Download', new_data.to_csv(index=False, sep=';'), f'{file_name}.csv', 'text/csv', key='download-csv'
+        st.write(f'{data.shape[0]} texts will be translated')
+        st.dataframe(data.reset_index(drop=True))
+    translate = st.button(
+        'Press the button to translate the data', on_click=make_more_columns, args=(data, lang, to_translate)
     )
+
+    if translate or st.session_state.load_state:
+        st.session_state.load_state = True
+
+        translated_data = st.session_state.translated_df
+        st.success('Translation finished!', icon='✅')
+        st.dataframe(translated_data)
+        st.subheader('Customize translated data')
+        options = st.multiselect(
+            label='Columns to use',
+            options=list(translated_data.columns),
+            default=['Stem', 'Word', 'Sentence'] + [col for col in translated_data.columns if 'translated' in col],
+            help='Select the columns you want to keep',
+        )
+
+        my_expander1 = st.expander(label='Rename columns')
+        with my_expander1:
+            # possibility to rename the columns
+            new_col_names = {}
+            for col in options:
+                new_name = st.text_input(f'{col} name', f'{col}', help=f'Write a new {col} name')
+                new_col_names[col] = new_name
+        # downloading
+        new_data = translated_data[options].rename(columns=new_col_names)
+        highlight = st.selectbox(
+            label='Select highlight options',
+            options=('None', 'Replace with underscore', 'Surround with [] brackets', 'Surround with {} brackets'),
+            index=0,
+            help='separator',
+        )
+        if highlight is None:
+            new_data['sentence_with_highlight'] = new_data['Sentence']
+        elif highlight == 'Replace with underscore':
+            new_data['sentence_with_highlight'] = new_data.apply(lambda x: x.Sentence.replace(x.Word, '_'), axis=1)
+        elif highlight == 'Surround with [] brackets':
+            new_data['sentence_with_highlight'] = new_data.apply(
+                lambda x: x.Sentence.replace(x.Word, f'[{x.Word}]'), axis=1
+            )
+        elif highlight == 'Surround with {} brackets':
+            new_data['sentence_with_highlight'] = new_data.apply(
+                lambda x: x.Sentence.replace(x.Word, f'{{{x.Word}}}'), axis=1
+            )
+        st.dataframe(new_data)
+
+        st.subheader('Download options')
+        keep_header = st.checkbox('Keep header', value=False)
+        sep = st.selectbox(label='Select separator', options=(';', 'Tab'), help='separator')
+        sep = sep if sep == ';' else '\t'
+
+        file_name = st.text_input('File name (without extension)', 'anki_table')
+        st.download_button(
+            label='Press to Download',
+            data=new_data.to_csv(index=False, sep=';', header=not keep_header),
+            file_name=f'{file_name}.csv',
+            mime='text/csv',
+            key='download-csv',
+            help='press m!',
+        )
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
+import datetime
 import sqlite3
 import tempfile
 from typing import List
 
+import altair as alt
 import pandas as pd
 import streamlit as st
 from deep_translator import GoogleTranslator
@@ -36,10 +38,14 @@ def get_data_from_vocab(db: st.runtime.uploaded_file_manager.UploadedFile) -> pd
     """
 
     cur.execute(sql)
-    data = cur.fetchall()
+    data_sql = cur.fetchall()
     data = pd.DataFrame(
-        data, columns=['stem', 'word', 'word_lang', 'example', 'book_title', 'book_authors', 'timestamp']
+        data_sql, columns=['Stem', 'Word', 'Word language', 'Sentence', 'Book title', 'Authors', 'Timestamp']
     )
+    data['Timestamp'] = data['Timestamp'].apply(
+        lambda t: datetime.datetime.fromtimestamp(t / 1000).strftime('%Y-%m-%d %H:%M:%S')
+    )
+    data = data.sort_values('Timestamp').reset_index(drop=True)
     return data
 
 
@@ -58,9 +64,11 @@ def translate(data: List, lang: str) -> List[str]:
     translated = []
     for text_lang, text in stqdm(data, total=len(data), desc='Translating...'):
         translated.append(GoogleTranslator(source=text_lang, target=lang).translate(text))
+
     return translated
 
 
+@st.cache(suppress_st_warning=True)
 def make_more_columns(data: pd.DataFrame, lang: str, to_translate: List[str]) -> pd.DataFrame:
     """
     Create additional columns.
@@ -68,6 +76,7 @@ def make_more_columns(data: pd.DataFrame, lang: str, to_translate: List[str]) ->
     Args:
         data: pandas DataFrame with the data
         lang: target language for translation
+        to_translate: columns to translate
 
     Returns:
         processed data.
@@ -75,12 +84,66 @@ def make_more_columns(data: pd.DataFrame, lang: str, to_translate: List[str]) ->
     """
     for col in to_translate:
         data[f'translated_{col.lower()}'] = translate(
-            list(data[['word_lang', 'word']].itertuples(index=False, name=None)), lang
+            list(data[['Word language', col]].itertuples(index=False, name=None)), lang
         )
 
-    data['sentence_with_brackets'] = data.apply(lambda x: x.example.replace(x.word, f'{{{x.word}}}'), axis=1)
-    data['sentence_with_different_brackets'] = data.apply(lambda x: x.example.replace(x.word, f'[{x.word}]'), axis=1)
+    data['sentence_with_highlight'] = data.apply(lambda x: x.Sentence.replace(x.Word, '_'), axis=1)
     data['sentence_with_cloze'] = data.apply(
-        lambda x: x.example.replace(x.word, f'{{c1::{x.translated_word}}}'), axis=1
+        lambda x: x.Sentence.replace(x.Word, f'{{c1::{x.translated_word}}}'), axis=1
     )
+    st.session_state.translated_df = data.reset_index(drop=True)
     return data
+
+
+def show_vocabulary_stats(df: pd.DataFrame) -> None:
+    """
+    Show various statistics based on the data.
+
+    Args:
+        df: dataframe with data
+
+    Returns:
+        Nothing
+    """
+    df['date'] = pd.to_datetime(df['Timestamp']).dt.date
+    d = df.groupby('date')['Sentence'].count().sort_index().reset_index()
+    d['count'] = d['Sentence'].cumsum()
+
+    chart = (
+        alt.Chart(d)
+        .mark_line(point=True, strokeWidth=1)
+        .encode(x=alt.X('date:T', timeUnit='yearmonthdate'), y='count:Q')
+        .configure_point(size=20)
+        .properties(title='Number of word over time')
+        .interactive()
+    )
+
+    d = (
+        df['Book title']
+        .value_counts()
+        .reset_index()
+        .rename(columns={'index': 'Book title', 'Book title': 'Count'})
+        .head(5)
+    )
+
+    chart1 = (
+        alt.Chart(d)
+        .mark_bar()
+        .encode(y='Book title', x='Count')
+        .properties(title='Number of words in top 5 books')
+        .interactive()
+    )
+    col1_, col2_ = st.columns(2)
+    with col1_:
+        st.altair_chart(chart)
+    with col2_:
+        st.altair_chart(chart1)
+
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label='Number of words', value=df.shape[0], help='Number of unique words in the vocabulary')
+    col2.metric(
+        label='Number of books', value=df['Book title'].nunique(), help='Number of unique books in the vocabulary'
+    )
+    col3.metric(
+        label='Number of reading days', value=df['date'].nunique(), help='Number of days with at least one word marked'
+    )
